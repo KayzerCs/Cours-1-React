0b71cb9fe65a718bc7b1d63b376cc2da7c5f0015
Kayzer <max.mlv2210@gmail.com>
2024-06-06 12:34:02 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
+        debitis cum nesciunt, consequatur rem aspernatur alias sapiente ut
+        pariatur expedita repudiandae voluptates itaque delectus obcaecati
+        possimus error nisi odio vero! Quam ducimus consequuntur, earum
+        quibusdam vitae dolorum ratione saepe repudiandae commodi quod! Illum
+        esse cupiditate sapiente ipsum rerum. Ut ducimus, doloremque laborum
+        modi quia, in non, culpa aliquam saepe odit optio esse sapiente error
+        repellat quaerat neque beatae expedita voluptates cupiditate nobis
+        recusandae. Illo maiores voluptatum eum molestias ut veniam nisi quo
+        inventore atque laborum? Animi molestias expedita dolores corporis
+        quaerat dignissimos dolorum officia saepe. Quam labore distinctio
+        aspernatur autem!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit
+        aliquam at hic nisi corporis quam culpa quasi sint molestiae autem dolor
+        beatae alias, debitis laboriosam similique commodi mollitia officiis
+        ratione! Cupiditate a corporis illo omnis mollitia recusandae maiores
+        quam voluptates, reprehenderit cumque nemo. Nam a id voluptate dolor
+        error, repellat assumenda nihil voluptates amet ipsa inventore quas?
+        Impedit quis eveniet rem ipsa autem nam alias minus id quisquam commodi.
+        Corrupti voluptatum quaerat eius quam, molestias dolores quae quibusdam
+        id quis adipisci. Velit tenetur iure nobis a modi praesentium culpa
+        consectetur facere aut! Nostrum ipsa quae, dignissimos dolores sit minus
+        tempora.
+      </p>
+    </div>
+  );
+};
+
+export default About;
